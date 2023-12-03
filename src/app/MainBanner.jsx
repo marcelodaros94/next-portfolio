06c30c5a7288c37b391e34fd002a276ede08aa4e
Marcelo Da Ros <marcelodaros94@gmail.com>
2023-12-03 10:55:35 -0500

    Header

---
+++ src/app/MainBanner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MainBanner = () => {
+    return (
+        <section>
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+                    Hello, I'm Marcelo
+                </h1>
+                <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+                Over 7 years as a Fullstack Artist. And yes, passionate about UX, Marketing and Leadership too :)
+                </p>
+        </section>
+    )
+}
+
+export default MainBanner;
