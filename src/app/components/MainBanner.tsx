93fcf0b87e324bfd8ea56b1a45db329c80f04030
Marcelo Da Ros <marcelodaros94@gmail.com>
2023-12-14 15:25:42 -0500

    multi language banner

--- src/app/components/MainBanner.tsx
+++ src/app/components/MainBanner.tsx
@@ -1,16 +1,22 @@
+"use client"
+
 import React from "react";
 import Arrow from "./Arrow";
 import Link from "next/link";
+import { useContent } from "../context/ContentContext";
 
 const MainBanner = () => {
+    const { language, data } = useContent();
+    const { title, description } = data[language];
+
     return (
         <section className="bg-[#121212]  flex items-center justify-center">
             <div>
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-                    Hola, soy Marcelo
+                    { title }
                 </h1>
                 <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-                Más de 7 años como artista Fullstack. Y sí, apasionado también por el UX, Marketing y Liderazgo ;)
+                    { description }
                 </p>
                 <Link href="https://github.com/marcelodaros94" target="_blank" className="block mx-auto text-center border border-solid border-gray-300  text-base sm:text-lg mb-6 lg:text-xl p-2 rounded-full">
                     Github code
