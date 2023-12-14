83d4bef46cbd8a394a8b7c203bc90530c2b1570e
Marcelo Da Ros <marcelodaros94@gmail.com>
2023-12-14 17:02:53 -0500

    justify text

--- src/app/components/MainBanner.tsx
+++ src/app/components/MainBanner.tsx
@@ -25,7 +25,7 @@ const MainBanner = () => {
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
                     { title }
                 </h1>
-                <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+                <p className="text-[#ADB7BE] text-justify text-base sm:text-lg mb-6 lg:text-xl">
                     { description }
                 </p>
                 <Link href="https://github.com/marcelodaros94" target="_blank" className="block mx-auto text-center border border-solid border-gray-300  text-base sm:text-lg mb-6 lg:text-xl p-2 rounded-full">
